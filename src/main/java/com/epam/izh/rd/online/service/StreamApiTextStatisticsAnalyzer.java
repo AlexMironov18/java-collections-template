cc97b7a2835831343f51ca80898b67a5245937a7
Alex Mironov <nomanyash@gmail.com>
2020-05-26 15:23:40 +0400

    tests are fixed using collection and stream.

--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,10 +2,12 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Collections.*;
 
@@ -16,36 +18,46 @@ import static java.util.Collections.*;
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        return Stream.of(text.replaceAll("\\W+", ""))
+                .mapToInt(String::length)
+                .sum();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        return (int) getWords(text).stream()
+                .count();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        return (int) getUniqueWords(text).stream()
+                .count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+        return Stream.of(text.split("\\W+"))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+        return getWords(text).stream()
+                .collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return emptyMap();
+        return getWords(text).stream().
+                collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        return getWords(text).stream()
+                .sorted(direction.equals(Direction.ASC) ? (Comparator.comparingInt(String::length)) :
+                        reverseOrder(Comparator.comparingInt(String::length)))
+                .collect(Collectors.toList());
     }
 }
